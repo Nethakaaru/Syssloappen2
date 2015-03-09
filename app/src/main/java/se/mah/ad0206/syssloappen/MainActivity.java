e0cdd308e30543fe7249baf743a59536ee68c225
Ollejan <jonas.dahlstrom1@gmail.com>
2015-03-09 12:12:34 +0100

    Test

--- app/src/main/java/se/mah/ad0206/syssloappen/MainActivity.java
+++ app/src/main/java/se/mah/ad0206/syssloappen/MainActivity.java
@@ -2,8 +2,14 @@ package se.mah.ad0206.syssloappen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    private String[] mMenuTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
