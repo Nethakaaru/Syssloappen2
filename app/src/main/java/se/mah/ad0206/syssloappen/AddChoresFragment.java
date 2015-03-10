9b7c5c5b046a05eab246b95b724dc44c5d2060b1
Nethakaaru <Nethakaaru@gmail.com>
2015-03-10 12:17:49 +0100

    Added language support, Commented in AddChoresFragment + minor fixes.

--- app/src/main/java/se/mah/ad0206/syssloappen/AddChoresFragment.java
+++ app/src/main/java/se/mah/ad0206/syssloappen/AddChoresFragment.java
@@ -15,15 +15,14 @@ import android.widget.Toast;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A fragment used to input chores.
+ * @author Sebastian Aspegren, Jonas Dahlström.
  */
 public class AddChoresFragment extends Fragment {
 
 
 
     private Spinner spinnerPoints;
-    private String[] arraySpinner;
-    private Button btnAddChore;
     private EditText etChoreName;
     private Controller controller;
 
@@ -31,32 +30,45 @@ public class AddChoresFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    /**
+     * OnCreateView method so the fragment actually works.
+     * @param inflater
+     *                  Inflater so we can inflate the fragment.
+     * @param container
+     *                  ViewGroup  for the inflation.
+     * @param savedInstanceState
+     *                              Bundle for the inflation.
+     * @return
+     *          the inflated view.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_chores, container, false);
 
-        this.arraySpinner = new String[] {"10", "20", "30", "40", "50", "100"};
+        //Populate the spinner with values and set the adapter.
+        String[] arraySpinner = new String[]{"5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "60", "70", "80", "90", "100"};
         spinnerPoints = (Spinner)view.findViewById(R.id.spinnerPoints);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(),
                 android.R.layout.simple_spinner_item, arraySpinner);
         spinnerPoints.setAdapter(adapter);
 
+        //Find some more components.
         etChoreName = (EditText)view.findViewById(R.id.etChoreName);
+        Button btnAddChore = (Button) view.findViewById(R.id.btnAddChore);
 
-        btnAddChore = (Button)view.findViewById(R.id.btnAddChore);
+        //clicklistener. If the text isn't empty save it to the database else message the user its empty.
         btnAddChore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(controller == null){
+                if (controller == null) {
                     Log.d("TEST", "CONTROLLER ÄR NULL!");
-                } else if(!etChoreName.getText().toString().equals("")) {
+                } else if (!etChoreName.getText().toString().equals("")) {
                     controller.btnAddChoreClicked(etChoreName.getText().toString(), spinnerPoints.getSelectedItem().toString());
                     etChoreName.setText("");
                 } else {
-                    Toast.makeText(getActivity(), "Skriv in ett namn", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), R.string.nameInput, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -64,6 +76,11 @@ public class AddChoresFragment extends Fragment {
         return view;
     }
 
+    /**
+     * Normal Controller setter
+     * @param controller
+     *                      reference to the controller.
+     */
     public void setController(Controller controller) {
         this.controller = controller;
     }
