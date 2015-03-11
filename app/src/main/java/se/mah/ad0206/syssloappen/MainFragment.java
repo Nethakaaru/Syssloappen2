40ede1f8b1df25b328b4cdd86169c9316c5afebd
Nethakaaru <Nethakaaru@gmail.com>
2015-03-11 12:30:11 +0100

    fixed colon

--- app/src/main/java/se/mah/ad0206/syssloappen/MainFragment.java
+++ app/src/main/java/se/mah/ad0206/syssloappen/MainFragment.java
@@ -119,6 +119,6 @@ public class MainFragment extends Fragment {
      *              the users level.
      */
     public void setTVLevel(String text) {
-        TVLevel.setText(getResources().getString(R.string.points) +" "+ text + "/500");
+        TVLevel.setText(getResources().getString(R.string.pointsNoColon) +" "+ text + "/500");
     }
 }
