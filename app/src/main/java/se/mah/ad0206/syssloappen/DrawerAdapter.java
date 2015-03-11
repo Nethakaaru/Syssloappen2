a6b8004789554c2bfdc76bbec657e2789c7500f3
Nethakaaru <Nethakaaru@gmail.com>
2015-03-11 12:59:34 +0100

    added javadoc.

--- app/src/main/java/se/mah/ad0206/syssloappen/DrawerAdapter.java
+++ app/src/main/java/se/mah/ad0206/syssloappen/DrawerAdapter.java
@@ -8,26 +8,59 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 /**
- * Created by Sebastian Aspegren on 2015-03-09.
+ * Created by Sebastian Aspegren, Jonas Dahlström on 2015-03-09.
+ * A drawer for a wicked sick menu with animations.
  */
 public class DrawerAdapter extends ArrayAdapter<String> {
     private String[] pages;
 
-
+    /**
+     * Constructor. require the activity and a string with the titles of the fragments.
+     * @param context
+     *              the main activity.
+     * @param pages
+     *              the names of the fragments in a way the user would understand them.
+     */
     public DrawerAdapter(Context context, String[] pages) {
         super(context, R.layout.draweritem,pages);
         this.pages=pages;
     }
 
+    /**
+     * The getView method.
+     * @param position
+     *                  The position of the view.
+     * @param convertView
+     *                  The view..
+     * @param parent
+     *          The ViewGroup.
+     * @return
+     *          the view.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
+        //init viewHolder.
+        ViewHolder holder = new ViewHolder();
+        if(convertView==null) {
+            //inflate the layout.
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.draweritem, parent, false);
+            holder.item = (TextView) convertView.findViewById(R.id.DrawerItem);
+            convertView.setTag(holder);
 
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        convertView = inflater.inflate(R.layout.draweritem, parent, false);
-        TextView item  = (TextView) convertView.findViewById(R.id.DrawerItem);
-        item.setText(pages[position]);
+        }else{
+            holder=(ViewHolder) convertView.getTag();
+        }
 
+        holder.item.setText(pages[position]);
         return convertView;
     }
 
-}
+
+    /**
+     * A ViewHolder class containing a textView.
+     */
+    private static class ViewHolder {
+            TextView item;
+    }
+    }
