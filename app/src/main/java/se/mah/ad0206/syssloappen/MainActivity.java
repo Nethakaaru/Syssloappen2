54a614216c473568323824f193febb5fceec4d5e
Nethakaaru <Nethakaaru@gmail.com>
2015-03-12 13:42:22 +0100

    A metric shit ton of javadoc.

--- app/src/main/java/se/mah/ad0206/syssloappen/MainActivity.java
+++ app/src/main/java/se/mah/ad0206/syssloappen/MainActivity.java
@@ -16,60 +16,72 @@ public class MainActivity extends ActionBarActivity {
     private android.support.v7.app.ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
-
+    /**
+     * The main activity's onCreate method. It starts the controller and the drawer menu.
+     * @param savedInstanceState
+     *                          I have no idea what this does.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         controller = new Controller(this);
+        //find components.
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         ListView mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
-
+        //set menu icon.
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_drawer);
 
-        mDrawerToggle = new android.support.v7.app.ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name,R.string.app_name);
-
+        //Construct the action drawer, set it as a listener for the layout and set some stuff.
+        mDrawerToggle = new android.support.v7.app.ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawerOpen,R.string.drawerClose);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        //The text in the manu.
         String[] pages = new String[]{"Huvudsidan", "Lägg till syssla", "Ta bort syssla", "Historik"};
         DrawerAdapter drawerAdapter = new DrawerAdapter(this, pages);
         mDrawerList.setAdapter(drawerAdapter);
-
+        //If it is clicked...
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Message the controller and close the menu.
                 controller.drawerItemClicked(position, view);
                 mDrawerLayout.closeDrawers();
             }
         });
     }
 
+    /**
+     * A method that is activated when the options item is clicked.
+     * @param item
+     *              the menu item
+     * @return
+     *          if it has handled the click or not.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle your other action bar items...
-
-        return super.onOptionsItemSelected(item);
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
+    /**
+     * If the user clicks the back button...
+     */
     @Override
     public void onBackPressed() {
+        //Warn them that they are about to exit the app.
         new AlertDialog.Builder(this)
-                .setTitle("Avsluta applikation")
-                .setMessage("Är du säker på att du vill avsluta Syssloappen?")
+                .setTitle(R.string.exitApp)
+                .setMessage(R.string.exitAppConfirm)
+                //if yes, close the app.
                 .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         MainActivity.super.onBackPressed();
                     }
                 })
+                //if no...
                 .setNegativeButton("Nej", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // Do nothing
